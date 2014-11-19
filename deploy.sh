0a2d38cfadd75e268026982c4720d2d5fbbc9eff
Rob Hogan <rh389@cam.ac.uk>
2014-11-19 20:19:35 +0000

    Upload dockercfg

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ EB_BUCKET=elasticbeanstalk-eu-west-1-937007889776
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region=eu-west-1
+aws s3 cp .dockercfg s3://$EB_BUCKET/.dockercfg --region=eu-west-1
+
 aws elasticbeanstalk create-application-version --application-name docker-deploy \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE --region=eu-west-1
 
